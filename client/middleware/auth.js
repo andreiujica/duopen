b171645388bd710a5e59047a632dfb952d059dcf
Andrei Ujica <andrei@vipra.tech>
2023-08-22 19:25:41 +0100

    fix(client): get rid of auth guards during pre-rendering

--- client/middleware/auth.js
+++ client/middleware/auth.js
@@ -3,6 +3,9 @@ import { useUserStore } from '~/stores/user';
 export default defineNuxtRouteMiddleware((to, from) => {
     const userStore = useUserStore();
 
+    // If we're in build mode (i.e., prerendering), skip the middleware check.
+    if (process.server && process.static) return;
+
     // Check if username or avatarColor is undefined or null
     if (!userStore.username || !userStore.avatarColor) {
         // Redirect to login or any other desired page
--- client/middleware/check-room.js
+++ client/middleware/check-room.js
@@ -3,6 +3,9 @@ import { useRoomsStore } from '~/stores/rooms';
 export default defineNuxtRouteMiddleware((to, from) => {
     const roomsStore = useRoomsStore();
 
+    // If we're in build mode (i.e., prerendering), skip the middleware check.
+    if (process.server && process.static) return;
+
     // Check if username or avatarColor is undefined or null
     if (!roomsStore.getCurrentRoom()) {
         // Redirect to join-room or any other desired page
