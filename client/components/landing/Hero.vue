708efd9b9f7a178ad20a137eaa88b0d832c5c2b0
Andrei Ujica <andrei@vipra.tech>
2023-08-23 21:48:36 +0100

    chore(client): try to fix image not rendering by changing it with a random jpg

--- client/components/landing/Hero.vue
+++ client/components/landing/Hero.vue
@@ -31,7 +31,7 @@ const currentHeroScreenshot = computed(() => {
     </UButton>
     <ClientOnly>
       <img
-        src="images/random-pixels.jpg"
+        src="/images/random-pixels.jpg"
         alt="Duopen App Screenshot"
         class="mt-4 w-11/12 sm:w-5/6 sm:shadow-2xl dark:shadow-gray-200 rounded-xl border border-gray-200 dark:border-gray-700"
       />
